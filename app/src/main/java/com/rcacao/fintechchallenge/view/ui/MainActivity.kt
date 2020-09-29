e179d6afecde247f201b9125b6f210c6f70556d5
renan <rcacao10@gmail.com>
2020-09-29 01:06:44 -0300

    move MainActivity

--- app/src/main/java/com/rcacao/fintechchallenge/view/ui/MainActivity.kt
+++ app/src/main/java/com/rcacao/fintechchallenge/view/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.rcacao.fintechchallenge
+package com.rcacao.fintechchallenge.view.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rcacao.fintechchallenge.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
