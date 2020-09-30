4272669c95205b671dbee9fcc0595ecac5146272
renan <rcacao10@gmail.com>
2020-09-30 00:24:15 -0300

    adiciona nomes serializados ao objeto Contact

--- app/src/main/java/com/rcacao/fintechchallenge/data/model/Contact.kt
+++ app/src/main/java/com/rcacao/fintechchallenge/data/model/Contact.kt
@@ -1,9 +1,14 @@
 package com.rcacao.fintechchallenge.data.model
 
+import com.google.gson.annotations.SerializedName
+
 class Contact {
 
+    @SerializedName("name")
     val name: String = ""
+    @SerializedName("username")
     val userName: String = ""
+    @SerializedName("img")
     val imageUrl: String = ""
 
 }
