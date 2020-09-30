d7a434b4cc460971dd9f78e35a673168ffe2811d
renan <rcacao10@gmail.com>
2020-09-29 22:39:45 -0300

    adiciona glide ao projeto

---
+++ app/src/main/java/com/rcacao/fintechchallenge/view/AppGlideModule.kt
@@ -0,0 +1,7 @@
+package com.rcacao.fintechchallenge.view
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+class AppGlideModule : AppGlideModule()
