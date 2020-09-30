f4ebc771f52e31d3eb3d92f41d260ac2613e062e
renan <rcacao10@gmail.com>
2020-09-29 22:50:32 -0300

    cria view e metodos de binding que complementam a tela de lista de contatos

--- app/src/main/java/com/rcacao/fintechchallenge/view/ui/BindingAdapter.kt
+++ app/src/main/java/com/rcacao/fintechchallenge/view/ui/BindingAdapter.kt
@@ -1,10 +1,14 @@
 package com.rcacao.fintechchallenge.view.ui
 
+import android.widget.Button
 import android.widget.ImageView
+import android.widget.ProgressBar
+import androidx.core.view.isVisible
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.rcacao.fintechchallenge.data.model.Contact
 import com.rcacao.fintechchallenge.view.GlideApp
+import com.rcacao.fintechchallenge.view.uistate.ContactsUiState
 
 
 @BindingAdapter("bind:imageUrl")
@@ -20,6 +24,30 @@ fun setContacts(recyclerView: RecyclerView, contacts: List<Contact>?) {
     recyclerView.adapter = ContactsAdapter().apply {
         this.contacts = contacts ?: emptyList()
         notifyDataSetChanged()
+    }
+}
+
+@BindingAdapter("bind:stateVisibility")
+fun setVisibility(progressBar: ProgressBar, uiState: ContactsUiState?) {
+    when (uiState) {
+        is ContactsUiState.Loading -> progressBar.isVisible = true
+        else -> progressBar.isVisible = false
+    }
+}
+
+@BindingAdapter("bind:stateVisibility")
+fun setVisibility(recyclerView: RecyclerView, uiState: ContactsUiState?) {
+    when (uiState) {
+        is ContactsUiState.Loaded -> recyclerView.isVisible = true
+        is ContactsUiState.Error -> recyclerView.isVisible = false
+    }
+}
+
+@BindingAdapter("bind:stateVisibility")
+fun setVisibility(button: Button, uiState: ContactsUiState?) {
+    when (uiState) {
+        is ContactsUiState.Error -> button.isVisible = true
+        else -> button.isVisible = false
     }
 }
 
